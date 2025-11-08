e3ca2f39834fbf58bd938c26ddb0216f154d77bb
malbroo96 <akhiljoseph225292@gmail.com>
2025-11-08 12:46:54 +0530

    hello world created

---
+++ health_tracker_v2/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (<div>
+    <h1 className='bg bg-red-600 text-gray-500'>hello world</h1>
+  </div>
+   
+  )
+}
+
+
+
+export default App
