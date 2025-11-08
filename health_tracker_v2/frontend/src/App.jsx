d8bef65dd4e660bf9834dad19848c06f4594d9d3
malbroo96 <akhiljoseph225292@gmail.com>
2025-11-08 13:24:17 +0530

    basic login ui

--- health_tracker_v2/frontend/src/App.jsx
+++ health_tracker_v2/frontend/src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  return (<div>
-    <h1 className='bg bg-red-600 text-gray-500'>hello world</h1>
-  </div>
-   
-  )
+  const handleSubmit=()=>{
+    console.log("Username:", username);
+    console.log("Password:", password);
+    alert(`Username: ${username}\nPassword: ${password}`);
+  }
+
+  return (
+    <div>
+      <input onChange={(e)=>setUsername(e.target.value)} type="text" class="px-4 py-2 leading-tight" placeholder="Username" />
+      
+      <input onChange={(a)=>setPassword(a.target.value)}
+        type="text"
+        class="px-4 py-2 leading-tight"
+        placeholder="password"
+      />
+      <button onClick={handleSubmit} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+    </div>
+  );
 }
 
-
-
-export default App
+export default App;
